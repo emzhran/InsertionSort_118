18ff96d3b38c31ccae06450b792a5ce184fd9761
emzhran <zachran750@gmail.com>
2023-03-23 15:20:37 +0700

    penambahan void input

--- InsortedSort/InsortedSort.cpp
+++ InsortedSort/InsortedSort.cpp
@@ -2,6 +2,35 @@
 //
 
 #include <iostream>
+using namespace std;
+
+int arr[20];
+int n;
+
+void input() {
+    while (true)
+    {
+        cout << "Masukkan Jumlah Data Pada Array : ";
+        cin >> n;
+
+        if (n <= 20)
+        {
+            break;
+        }
+        else
+        {
+            cout << "\nArray yang anda masukkan maksimal 20 element.\n";
+        }
+    }
+    cout << endl;
+    cout << "======================" << endl;
+    cout << "Masukkan Element Array" << endl;
+    cout << "======================" << endl;
+
+    for (int i = 0; i < n; i++) {
+        cout << "Data ke-" << (i + 1) << ": ";
+    }
+}
 
 int main()
 {
